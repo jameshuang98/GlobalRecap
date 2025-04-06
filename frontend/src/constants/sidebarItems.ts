1210c3ce2ec546f74fa4b95dfdafae09d489a0ac
James <jameshuang337@gmail.com>
2025-04-05 23:29:01 -0700

    create constant array of side bar items

---
+++ frontend/src/constants/sidebarItems.ts
@@ -0,0 +1,11 @@
+import { Home, Landmark, Briefcase, Lightbulb, Dumbbell, Clapperboard, Earth } from "lucide-react"
+
+export const sidebarItems = [
+    { title: "Home", url: "#", icon: Home },
+    { title: "Politics", url: "#", icon: Landmark },
+    { title: "Business", url: "#", icon: Briefcase },
+    { title: "Technology", url: "#", icon: Lightbulb },
+    { title: "Sports", url: "#", icon: Dumbbell },
+    { title: "Entertainment", url: "#", icon: Clapperboard },
+    { title: "Environment", url: "#", icon: Earth }
+]
