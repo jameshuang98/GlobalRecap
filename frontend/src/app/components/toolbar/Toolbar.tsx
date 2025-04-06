cf54a2f7ffa6274e40b66a83015640488bb5725d
James <jameshuang337@gmail.com>
2025-04-06 11:08:54 -0700

    refactor Toolbar component to use DropdownMenu

--- frontend/src/app/components/toolbar/Toolbar.tsx
+++ frontend/src/app/components/toolbar/Toolbar.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import { Menu } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 
 const Toolbar = () => {
     return (
         <div className="flex items-center justify-between px-3 py-1 shadow-md">
             <div>GlobalRecap</div>
-            <Button variant="ghost" size="icon">
-                <Menu />
-            </Button>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                        <Menu />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem>Archive</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     )
 }
