5776a7a9d37b66da389d11725eba20087f3cc869
James <jameshuang337@gmail.com>
2025-04-05 23:34:11 -0700

    fix background color for tag (potentially caused by tailwind JIT)

--- frontend/src/app/components/news-card/index.tsx
+++ frontend/src/app/components/news-card/index.tsx
@@ -29,7 +29,7 @@ const NewsCard: React.FC<NewsCardProps> = ({ title, summary, category, tags, art
         <div className='flex flex-col justify-between'>
           <div className='space-x-1'>
             {tags && tags.map((tag, index) => (
-              <Badge key={index} className={`text-xs font-normal my-1 rounded-xl bg-[${tagColors[tag]}]`} >
+              <Badge key={index} className={`text-xs font-normal my-1 rounded-xl`} style={{ backgroundColor: tagColors[tag] }} >
                 {tag}
               </Badge>
             ))}
