095693a476b18bb364785b68aa6496485d638101
James <jameshuang337@gmail.com>
2025-04-05 23:30:34 -0700

    create NewsCategory enum

---
+++ frontend/src/models/enums/news-category.ts
@@ -0,0 +1,9 @@
+export enum NewsCategory {
+  Politics = "Politics",
+  Business = "Business",
+  Technology = "Technology",
+  Sports = "Sports",
+  Health = "Health",
+  Entertainment = "Entertainment",
+  Environment = "Environment",
+}
